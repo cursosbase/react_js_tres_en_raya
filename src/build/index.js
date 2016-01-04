b712a1a4e2977788ad9bac5361694c729ceb40e9
Cursos Base <contacto@cursosbase.com>
2016-01-04 16:07:43 +0100

    comprobar ganador

--- src/build/index.js
+++ src/build/index.js
@@ -17,13 +17,59 @@ var App = React.createClass({
   },
 
   appClick: function appClick(numeroFila, numberoColumna) {
-    var valores = this.state.valores;
-    var nuevoValor = this.state.turno === JUGADORX ? 'X' : '0';
-    valores[numeroFila][numberoColumna] = nuevoValor;
-    this.setState({
-      turno: this.state.turno === JUGADORX ? JUGADOR0 : JUGADORX,
-      valores: this.state.valores
-    });
+    if (!this.state.hayGanador) {
+      var valores = this.state.valores;
+      var nuevoValor = this.state.turno === JUGADORX ? 'X' : '0';
+      valores[numeroFila][numberoColumna] = nuevoValor;
+
+      var hayGanador = this.comprobarGanador(valores, nuevoValor);
+      if (hayGanador) {
+        alert("Ha ganado: " + this.state.turno);
+      }
+      this.setState({
+        turno: this.state.turno === JUGADORX ? JUGADOR0 : JUGADORX,
+        valores: this.state.valores,
+        hayGanador: hayGanador ? this.state.turno : false
+      });
+    }
+  },
+
+  comprobarGanador: function comprobarGanador(vals, newVal) {
+    var nRow, nCol;
+    // comprobar filas
+    for (nRow = 0; nRow < 3; nRow++) {
+      for (nCol = 0; nCol < 3; nCol++) {
+        if (vals[nRow][nCol] != newVal) {
+          break;
+        }
+        if (nCol == 2) {
+          return true;
+        }
+      }
+    }
+
+    // comprobar columnas
+    for (nCol = 0; nCol < 3; nCol++) {
+      for (nRow = 0; nRow < 3; nRow++) {
+        if (vals[nRow][nCol] != newVal) {
+          break;
+        }
+        if (nRow == 2) {
+          return true;
+        }
+      }
+    }
+
+    // Comprobar diagonal
+    if (vals[0][0] == newVal && vals[1][1] == newVal && vals[2][2] == newVal) {
+      return true;
+    }
+    // Comprobar anti-diagonal
+    if (vals[0][2] == newVal && vals[1][1] == newVal && vals[2][0] == newVal) {
+      return true;
+    }
+
+    return false;
   },
 
   render: function render() {
--- src/js/App.jsx
+++ src/js/App.jsx
@@ -17,14 +17,60 @@ var App = React.createClass({
   },
 
   appClick: function(numeroFila, numberoColumna){
-    let valores = this.state.valores;
-    let nuevoValor = this.state.turno === JUGADORX ? 'X':'0';
-    valores[numeroFila][numberoColumna] = nuevoValor;
-    this.setState({
-      turno: this.state.turno === JUGADORX ? JUGADOR0:JUGADORX,
-      valores: this.state.valores
-    });
+    if(!this.state.hayGanador){
+      let valores = this.state.valores;
+      let nuevoValor = this.state.turno === JUGADORX ? 'X':'0';
+      valores[numeroFila][numberoColumna] = nuevoValor;
+
+      let hayGanador = this.comprobarGanador(valores, nuevoValor);
+      if(hayGanador){
+        alert("Ha ganado: " + this.state.turno);
+      }
+      this.setState({
+        turno: this.state.turno === JUGADORX ? JUGADOR0:JUGADORX,
+        valores: this.state.valores,
+        hayGanador: hayGanador ? this.state.turno:false
+      });
+    }
   },
+
+  comprobarGanador: function(vals, newVal){
+		var nRow, nCol;
+		// comprobar filas
+		for (nRow = 0; nRow < 3; nRow++) {
+		    for (nCol = 0; nCol < 3; nCol++) {
+			    if (vals[nRow][nCol]!=newVal) {
+			        break;
+			    }
+			    if (nCol == 2) {
+			        return true;
+			    }
+		  }
+		}
+
+		// comprobar columnas
+		for (nCol = 0; nCol < 3; nCol++) {
+		    for (nRow = 0; nRow < 3; nRow++) {
+			    if (vals[nRow][nCol]!=newVal) {
+			        break;
+			    }
+			    if (nRow == 2) {
+			        return true;
+			    }
+		  }
+		}
+
+		// Comprobar diagonal
+	  if (vals[0][0]==newVal && vals[1][1]==newVal && vals[2][2]==newVal) {
+	    return true;
+	  }
+		// Comprobar anti-diagonal
+	  if (vals[0][2]==newVal && vals[1][1]==newVal && vals[2][0]==newVal) {
+	    return true;
+	  }
+
+		return false;
+  	},
 
   render: function(){
     var texto;
